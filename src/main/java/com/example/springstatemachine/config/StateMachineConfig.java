4f366ebb2440b7c537a129eea84c73e10fa3bd9e
Max Dokuchaev <mixamus@icloud.com>
2021-10-06 22:25:10 +0300

    Added green flow

--- src/main/java/com/example/springstatemachine/config/StateMachineConfig.java
+++ src/main/java/com/example/springstatemachine/config/StateMachineConfig.java
@@ -2,30 +2,35 @@ package com.example.springstatemachine.config;
 
 import com.example.springstatemachine.domain.Events;
 import com.example.springstatemachine.domain.States;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
 import org.springframework.statemachine.config.EnableStateMachine;
 import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.listener.StateMachineListener;
+import org.springframework.statemachine.listener.StateMachineListenerAdapter;
+import org.springframework.statemachine.state.State;
+import org.springframework.statemachine.transition.Transition;
 
+@Slf4j
 @Configuration
 @EnableStateMachine
 public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<States, Events> {
 
   @Override
-  public void configure(
-      StateMachineConfigurationConfigurer
-          <States, Events> config) throws Exception {
+  public void configure(StateMachineConfigurationConfigurer<States, Events> config) throws Exception {
 
     config.withConfiguration()
+        .listener(listener())
         .autoStartup(true);
   }
 
   @Override
-  public void configure(
-      StateMachineStateConfigurer<States, Events> states)
-      throws Exception {
+  public void configure(StateMachineStateConfigurer<States, Events> states) throws Exception {
 
     states.withStates()
         .initial(States.BACKLOG)
@@ -35,9 +40,7 @@ public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<States
   }
 
   @Override
-  public void configure(
-      StateMachineTransitionConfigurer<States, Events> transitions)
-      throws Exception {
+  public void configure(StateMachineTransitionConfigurer<States, Events> transitions) throws Exception {
 
     transitions.withExternal()
         .source(States.BACKLOG)
@@ -58,5 +61,27 @@ public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<States
         .source(States.TESTING)
         .target(States.DONE)
         .event(Events.QA_TEAM_APPROVE);
+  }
+
+  private StateMachineListener<States, Events> listener() {
+    return new StateMachineListenerAdapter<States, Events>() {
+      @Override
+      public void transition(Transition<States, Events> transition) {
+        log.warn("move from:{} to:{}",
+            ofNullableState(transition.getSource()),
+            ofNullableState(transition.getTarget()));
+      }
+
+      @Override
+      public void eventNotAccepted(Message<Events> event) {
+        log.error("event not accepted: {}", event);
+      }
+
+      private Object ofNullableState(State s) {
+        return Optional.ofNullable(s)
+            .map(State::getId)
+            .orElse(null);
+      }
+    };
   }
 }
--- src/test/java/com/example/springstatemachine/SpringStateMachineApplicationTests.java
+++ src/test/java/com/example/springstatemachine/SpringStateMachineApplicationTests.java
@@ -1,13 +1,47 @@
 package com.example.springstatemachine;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.springstatemachine.domain.Events;
+import com.example.springstatemachine.domain.States;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
 
 @SpringBootTest
 class SpringStateMachineApplicationTests {
 
+  @Autowired
+  private StateMachine<States, Events> stateMachine;
+
   @Test
-  void contextLoads() {
+  void initTest() {
+
+    assertThat(stateMachine.getState().getId()).
+        isEqualTo(States.BACKLOG);
   }
 
+  @Test
+  void testGreenFlow() {
+    // Arrange & Act
+    stateMachine.sendEvent(Events.START_FEATURE);
+    stateMachine.sendEvent(Events.FINISH_FEATURE);
+    stateMachine.sendEvent(Events.QA_TEAM_APPROVE);
+
+    // Assert
+    assertThat(stateMachine.getState().getId())
+        .isEqualTo(States.DONE);
+  }
+
+  @Test
+  void testWrongWay() {
+    // Arrange & Act
+    stateMachine.sendEvent(Events.START_FEATURE);
+    stateMachine.sendEvent(Events.QA_TEAM_APPROVE);
+    // Asserts
+    assertThat(stateMachine.getState().getId())
+        .isEqualTo(States.IN_PROGRESS);
+  }
 }
